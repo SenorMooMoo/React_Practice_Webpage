e995ebeeb4d7498f093a7d060f543b529b54d57f
SenorMooMoo <35905949+SenorMooMoo@users.noreply.github.com>
2022-02-19 16:47:51 -0500

    Added react logo

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,16 @@
 import React from "react";
 export default function Main(){
-    return (<h1>Main content goes here</h1>)
+    return (
+        <main id="main">
+            <h1 id="main-title">Fun facts about React</h1>
+            <ul id="main-list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+            <img src={require("../images/react-icon-large.png")} id="react-body-img" alt={"react-logo"}/>
+        </main>
+    )
 }
